4c86bbb7dd322a07390fb0f5f9c3a3740f792d39
agib1 <arielegibson@gmail.com>
2023-11-06 10:50:56 +0000

    init

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,7 +1,6 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask
 
 from todo_app.flask_config import Config
-from todo_app.data.session_items import get_items, get_item, add_item, save_item, remove_item
 
 app = Flask(__name__)
 app.config.from_object(Config())
@@ -9,9 +8,4 @@ app.config.from_object(Config())
 
 @app.route('/')
 def index():
-    return render_template('index.html', items=get_items())
-
-@app.route('/add', methods=['POST'])
-def add():
-    add_item(request.form.get('title'))
-    return redirect('/')
+    return 'Hello World!'
